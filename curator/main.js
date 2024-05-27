d659b51b22eb33f6c9212df18abbb7ba6b2af2d8
iyaki <ivanyaki96@gmail.com>
2024-05-27 14:31:01 -0300

    Prueba action outputs

--- curator/main.js
+++ curator/main.js
@@ -5,12 +5,12 @@ import { addArticle } from './webpage.js';
 
 const LAST_ARTICLE_PATH = './data/last_article.txt'
 
-console.log('Starting curator')
 
 let existNewArticles = false
 
 let lastArticleId = fs.readFileSync(LAST_ARTICLE_PATH, { encoding: 'utf8' }).trim()
-for (let i = 1; i <= 3; ++i) {
+let i
+for (i = 1; i <= 3; ++i) {
 	const newArticle = await getNewArticle(lastArticleId)
 
 	if (newArticle === null) {
@@ -20,14 +20,11 @@ for (let i = 1; i <= 3; ++i) {
 	existNewArticles = true
 	addArticle(newArticle)
 	lastArticleId = newArticle.id
-
-	console.log(`Added ${i} articles`)
 }
+console.log(`Added ${i} articles`)
 
 
 if (existNewArticles) {
-	console.log('Updating last_article.txt')
+	// console.log('Updating last_article.txt')
 	fs.writeFileSync(LAST_ARTICLE_PATH, lastArticleId);
 }
-
-console.log('Curator finished')
